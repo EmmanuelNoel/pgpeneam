ec7af4b281a78845479b0aac269ede2e951bdd4e
EmmanuelNoel <jahounoel@gmail.com>
2022-11-30 23:59:47 +0100

    30/11/2022

---
+++ pgpeneam.sql
@@ -0,0 +1,667 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le : mer. 30 nov. 2022 à 23:35
+-- Version du serveur : 10.4.24-MariaDB
+-- Version de PHP : 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `pgpeneam`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `agent`
+--
+
+CREATE TABLE `agent` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(255) NOT NULL,
+  `prenom` varchar(255) NOT NULL,
+  `matricule` int(11) NOT NULL,
+  `passwd` varchar(255) NOT NULL,
+  `nationalite` varchar(255) NOT NULL,
+  `profession` varchar(255) NOT NULL,
+  `ifu` int(11) DEFAULT NULL,
+  `rib` int(11) DEFAULT NULL,
+  `email` varchar(255) NOT NULL,
+  `telephone` int(11) NOT NULL,
+  `adresse` varchar(255) NOT NULL,
+  `sexe` varchar(255) NOT NULL,
+  `en_service` tinyint(1) NOT NULL,
+  `date_premier_service` date NOT NULL,
+  `grade_id` varchar(255) NOT NULL,
+  `statut_id` int(11) NOT NULL,
+  `banque_id` int(11) NOT NULL,
+  `categorie_id` int(11) NOT NULL,
+  `role_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `agent`
+--
+
+INSERT INTO `agent` (`id`, `nom`, `prenom`, `matricule`, `passwd`, `nationalite`, `profession`, `ifu`, `rib`, `email`, `telephone`, `adresse`, `sexe`, `en_service`, `date_premier_service`, `grade_id`, `statut_id`, `banque_id`, `categorie_id`, `role_id`) VALUES
+(1, 'pifofuju@mailinator.com', 'syrukaf@mailinator.com', 17, '17pifofuju@mailinator.com', 'gupyz@mailinator.com', 'kizy@mailinator.com', 98, 65, 'juho@mailinator.com', 1, 'jareminy@mailinator.com', 'homme', 0, '1975-06-24', '9', 1, 2, 1, 1),
+(2, 'nuqy@mailinator.com', 'kyxipy@mailinator.com', 0, 'viqis@mailinator.comnuqy@mailinator.com', 'myhalezyvy@mailinator.com', 'xukifycij@mailinator.com', 25, 50, 'tapowux@mailinator.com', 1, 'bufux@mailinator.com', 'femme', 0, '2001-01-28', '1', 1, 1, 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `banque`
+--
+
+CREATE TABLE `banque` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `banque`
+--
+
+INSERT INTO `banque` (`id`, `nom`) VALUES
+(1, 'BOA'),
+(2, 'UBA');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `categorie`
+--
+
+CREATE TABLE `categorie` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `categorie`
+--
+
+INSERT INTO `categorie` (`id`, `nom`) VALUES
+(1, 'Enseignant'),
+(2, 'Administration');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `classe`
+--
+
+CREATE TABLE `classe` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(255) NOT NULL,
+  `filiere_id` int(11) NOT NULL,
+  `niveau` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `contrat`
+--
+
+CREATE TABLE `contrat` (
+  `id` int(11) NOT NULL,
+  `numcontrat` int(11) NOT NULL,
+  `date` date NOT NULL,
+  `agent_id` int(11) NOT NULL,
+  `entite_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `departement`
+--
+
+CREATE TABLE `departement` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(255) NOT NULL,
+  `entite_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `departement`
+--
+
+INSERT INTO `departement` (`id`, `nom`, `entite_id`) VALUES
+(1, 'Statistique', 1),
+(2, 'Planification', 1),
+(3, 'Management des organisations', 1),
+(4, 'Informatique de gestion', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `ecue`
+--
+
+CREATE TABLE `ecue` (
+  `id` int(11) NOT NULL,
+  `nom` int(11) NOT NULL,
+  `ue_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `entite`
+--
+
+CREATE TABLE `entite` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(255) NOT NULL,
+  `code` varchar(255) NOT NULL,
+  `directeur` varchar(255) NOT NULL,
+  `adresse` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `telephone` int(11) NOT NULL,
+  `universite_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `entite`
+--
+
+INSERT INTO `entite` (`id`, `nom`, `code`, `directeur`, `adresse`, `email`, `telephone`, `universite_id`) VALUES
+(1, 'Ecole Nationale d\'Economie Appliquee et de Management', 'ENEAM', 'Albert HONLONKOU', 'Gbemamey', 'eneam', 53536363, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `filiere`
+--
+
+CREATE TABLE `filiere` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(255) NOT NULL,
+  `departement_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `filiere`
+--
+
+INSERT INTO `filiere` (`id`, `nom`, `departement_id`) VALUES
+(1, 'Informatique de gestion', 4),
+(2, 'Statistique', 1),
+(4, 'Planification', 2),
+(5, 'Gestion des transports et logistiques', 3),
+(6, 'Gestion commerciale', 3),
+(7, 'Gestion des Banques et Assurance', 3),
+(8, 'Gestion Financiere et comptable', 3),
+(9, 'Gestion des ressources humaines', 3);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `grade`
+--
+
+CREATE TABLE `grade` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `grade`
+--
+
+INSERT INTO `grade` (`id`, `nom`) VALUES
+(1, 'E1'),
+(2, 'E2'),
+(3, 'E3'),
+(4, 'E4'),
+(5, 'M1'),
+(6, 'M2'),
+(7, 'M3'),
+(8, 'M4'),
+(9, 'C1'),
+(10, 'C2'),
+(11, 'C3'),
+(12, 'C4');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `niveau`
+--
+
+CREATE TABLE `niveau` (
+  `id` int(11) NOT NULL,
+  `libelle` varchar(255) NOT NULL,
+  `prix` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `niveau`
+--
+
+INSERT INTO `niveau` (`id`, `libelle`, `prix`) VALUES
+(1, 'Licence', 3500),
+(2, 'Master', 5000);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `prestation`
+--
+
+CREATE TABLE `prestation` (
+  `id` int(11) NOT NULL,
+  `contrat_id` int(11) NOT NULL,
+  `classe_id` int(11) NOT NULL,
+  `ecue_id` int(11) NOT NULL,
+  `massehoraire` int(11) NOT NULL,
+  `date_debut` date NOT NULL,
+  `date_fin` date NOT NULL,
+  `voulme_td` int(11) NOT NULL,
+  `volume_jour` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `role`
+--
+
+CREATE TABLE `role` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `role`
+--
+
+INSERT INTO `role` (`id`, `nom`) VALUES
+(1, 'Secretaire');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `statut`
+--
+
+CREATE TABLE `statut` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `statut`
+--
+
+INSERT INTO `statut` (`id`, `nom`) VALUES
+(1, 'Permanent'),
+(2, 'Vacataire');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `ue`
+--
+
+CREATE TABLE `ue` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(255) NOT NULL,
+  `entite_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `universite`
+--
+
+CREATE TABLE `universite` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(255) NOT NULL,
+  `code` varchar(255) NOT NULL,
+  `adresse` varchar(255) NOT NULL,
+  `logo` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `universite`
+--
+
+INSERT INTO `universite` (`id`, `nom`, `code`, `adresse`, `logo`) VALUES
+(1, 'Calavi', 'UAC', 'Abomey Calavi', 'C:/xampp/htdocs/pgpeneam/images/logo_uac2');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(255) NOT NULL,
+  `prenom` varchar(255) NOT NULL,
+  `sexe` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `fonction` varchar(255) NOT NULL,
+  `passwd` varchar(255) NOT NULL,
+  `role_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `agent`
+--
+ALTER TABLE `agent`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_agent_banque_id` (`banque_id`),
+  ADD KEY `fk_agent_statut_id` (`statut_id`),
+  ADD KEY `fk_agent_categorie_id` (`categorie_id`),
+  ADD KEY `fk_agent_role_id` (`role_id`);
+
+--
+-- Index pour la table `banque`
+--
+ALTER TABLE `banque`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `categorie`
+--
+ALTER TABLE `categorie`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `classe`
+--
+ALTER TABLE `classe`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_classe_filiere_id` (`filiere_id`),
+  ADD KEY `fk_classe_niveau` (`niveau`);
+
+--
+-- Index pour la table `contrat`
+--
+ALTER TABLE `contrat`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_contrat_agent_id` (`agent_id`),
+  ADD KEY `fk_contrat_entite_id` (`entite_id`);
+
+--
+-- Index pour la table `departement`
+--
+ALTER TABLE `departement`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_departement_entite_id` (`entite_id`);
+
+--
+-- Index pour la table `ecue`
+--
+ALTER TABLE `ecue`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_ecue_ue_id` (`ue_id`);
+
+--
+-- Index pour la table `entite`
+--
+ALTER TABLE `entite`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_entite_universite_id` (`universite_id`);
+
+--
+-- Index pour la table `filiere`
+--
+ALTER TABLE `filiere`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_filiere_departement_id` (`departement_id`);
+
+--
+-- Index pour la table `grade`
+--
+ALTER TABLE `grade`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `niveau`
+--
+ALTER TABLE `niveau`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `prestation`
+--
+ALTER TABLE `prestation`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_prestation_contrat_id` (`contrat_id`),
+  ADD KEY `fk_prestation_classe_id` (`classe_id`),
+  ADD KEY `fk_prestation_ecue_id` (`ecue_id`);
+
+--
+-- Index pour la table `role`
+--
+ALTER TABLE `role`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `statut`
+--
+ALTER TABLE `statut`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `ue`
+--
+ALTER TABLE `ue`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_ue_entite_id` (`entite_id`);
+
+--
+-- Index pour la table `universite`
+--
+ALTER TABLE `universite`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_user_role_id` (`role_id`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `agent`
+--
+ALTER TABLE `agent`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT pour la table `banque`
+--
+ALTER TABLE `banque`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT pour la table `categorie`
+--
+ALTER TABLE `categorie`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT pour la table `classe`
+--
+ALTER TABLE `classe`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `contrat`
+--
+ALTER TABLE `contrat`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `departement`
+--
+ALTER TABLE `departement`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT pour la table `ecue`
+--
+ALTER TABLE `ecue`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `entite`
+--
+ALTER TABLE `entite`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT pour la table `filiere`
+--
+ALTER TABLE `filiere`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT pour la table `grade`
+--
+ALTER TABLE `grade`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT pour la table `niveau`
+--
+ALTER TABLE `niveau`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT pour la table `prestation`
+--
+ALTER TABLE `prestation`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `role`
+--
+ALTER TABLE `role`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT pour la table `statut`
+--
+ALTER TABLE `statut`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT pour la table `ue`
+--
+ALTER TABLE `ue`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT pour la table `universite`
+--
+ALTER TABLE `universite`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT pour la table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `agent`
+--
+ALTER TABLE `agent`
+  ADD CONSTRAINT `fk_agent_banque_id` FOREIGN KEY (`banque_id`) REFERENCES `banque` (`id`),
+  ADD CONSTRAINT `fk_agent_categorie_id` FOREIGN KEY (`categorie_id`) REFERENCES `categorie` (`id`),
+  ADD CONSTRAINT `fk_agent_role_id` FOREIGN KEY (`role_id`) REFERENCES `role` (`id`),
+  ADD CONSTRAINT `fk_agent_statut_id` FOREIGN KEY (`statut_id`) REFERENCES `statut` (`id`);
+
+--
+-- Contraintes pour la table `classe`
+--
+ALTER TABLE `classe`
+  ADD CONSTRAINT `fk_classe_filiere_id` FOREIGN KEY (`filiere_id`) REFERENCES `filiere` (`id`),
+  ADD CONSTRAINT `fk_classe_niveau` FOREIGN KEY (`niveau`) REFERENCES `niveau` (`id`);
+
+--
+-- Contraintes pour la table `contrat`
+--
+ALTER TABLE `contrat`
+  ADD CONSTRAINT `fk_contrat_agent_id` FOREIGN KEY (`agent_id`) REFERENCES `agent` (`id`),
+  ADD CONSTRAINT `fk_contrat_entite_id` FOREIGN KEY (`entite_id`) REFERENCES `entite` (`id`);
+
+--
+-- Contraintes pour la table `departement`
+--
+ALTER TABLE `departement`
+  ADD CONSTRAINT `fk_departement_entite_id` FOREIGN KEY (`entite_id`) REFERENCES `entite` (`id`);
+
+--
+-- Contraintes pour la table `ecue`
+--
+ALTER TABLE `ecue`
+  ADD CONSTRAINT `fk_ecue_ue_id` FOREIGN KEY (`ue_id`) REFERENCES `ue` (`id`);
+
+--
+-- Contraintes pour la table `entite`
+--
+ALTER TABLE `entite`
+  ADD CONSTRAINT `fk_entite_universite_id` FOREIGN KEY (`universite_id`) REFERENCES `universite` (`id`);
+
+--
+-- Contraintes pour la table `filiere`
+--
+ALTER TABLE `filiere`
+  ADD CONSTRAINT `fk_filiere_departement_id` FOREIGN KEY (`departement_id`) REFERENCES `departement` (`id`);
+
+--
+-- Contraintes pour la table `prestation`
+--
+ALTER TABLE `prestation`
+  ADD CONSTRAINT `fk_prestation_classe_id` FOREIGN KEY (`classe_id`) REFERENCES `classe` (`id`),
+  ADD CONSTRAINT `fk_prestation_contrat_id` FOREIGN KEY (`contrat_id`) REFERENCES `contrat` (`id`),
+  ADD CONSTRAINT `fk_prestation_ecue_id` FOREIGN KEY (`ecue_id`) REFERENCES `ecue` (`id`);
+
+--
+-- Contraintes pour la table `ue`
+--
+ALTER TABLE `ue`
+  ADD CONSTRAINT `fk_ue_entite_id` FOREIGN KEY (`entite_id`) REFERENCES `entite` (`id`);
+
+--
+-- Contraintes pour la table `user`
+--
+ALTER TABLE `user`
+  ADD CONSTRAINT `fk_user_role_id` FOREIGN KEY (`role_id`) REFERENCES `role` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
