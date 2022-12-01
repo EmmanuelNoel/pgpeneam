3d4ad88b9cbb538cd42c74d1176a871fe764977a
EmmanuelNoel <jahounoel@gmail.com>
2022-12-01 17:28:54 +0100

    01/12/2022

--- connexion.php
+++ connexion.php
@@ -2,17 +2,56 @@
 
 
 
+
 if(!empty($_POST['login']))
 {
 
-    include 'connexionDB.php';
+    include ('connexionDB.php');
 
-    $donnees = $bdd->query('SELECT matricule,passwd FROM agent');
+    $tab_passe = [];
+    $tab_matricule = [];
+
+    $donneesmat = $bdd->query('SELECT matricule FROM agent');
+    $donneespasse = $bdd->query('SELECT passwd FROM agent');
 
     $matricule=$_POST['matricule'];
     $passe = $_POST['password'];
 
+    //tous les matricules de la table agent
+    $i=0;
+    while($infomat = $donneesmat->fetch())
+    {
+       $tab_matricule[$i] = $infomat['matricule'];
+       $i++;
+    }
+
+    //tous les mots de passe de la table agent
+
+    $i=0;
+    while($infopasse = $donneespasse->fetch())
+    {
+       $tab_passe[$i] = $infopasse['passwd'];
+       $i++;
+    }
+
+        
+    if(in_array($matricule,$tab_matricule))
+    {
+        if(in_array($passe,$tab_passe))
+        {
+           header('location:accueil.php');
+        }
+        else
+        {
+            header('location:index.php');
+        }
+    }
    
+    else
+    {
+        header('location:index.php');
+    }
+
 
 
 }
