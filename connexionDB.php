16f372bc5d33935a3816c64390527c4f8df68d84
Johanna <johannagng116@gmail.com>
2022-12-03 22:51:08 +0100

    nnnnn

--- connexionDB.php
+++ connexionDB.php
@@ -4,7 +4,7 @@
 try
 {
     // On se connecte à MySQL
-    $bdd = new PDO('mysql:host=localhost;dbname=pgpeneam', 'root', '');
+    $bdd = new PDO('mysql:host=localhost;dbname=pgpeneam', 'root', 'root');
 
 } catch (Exception $e) {
     // En cas d'erreur, on affiche un message et on arrête tout
