54014215d1978770bdc39c5a5aaaeb7f8a7354ec
Farren Lynda <lyndafarren22@gmail.com>
2022-12-19 09:49:19 +0100

    okay

--- traitement.php
+++ traitement.php
@@ -13,7 +13,7 @@ $enregistrer->execute(array(htmlspecialchars($_POST['nom']),htmlspecialchars($_P
 htmlspecialchars($passe),htmlspecialchars($_POST['nationalite']),htmlspecialchars($_POST['profession']),
 htmlspecialchars($_POST['ifu']),htmlspecialchars($_POST['rib']),htmlspecialchars($_POST['email']),
 htmlspecialchars($_POST['telephone']),htmlspecialchars($_POST['adresse']),htmlspecialchars($_POST['sexe']),
-htmlspecialchars($_POST['en_service']),
+htmlspecialchars($_POST['service']),
 htmlspecialchars($_POST['date_premier_service']),htmlspecialchars($_POST['grade']),htmlspecialchars($_POST['statut']),
 htmlspecialchars($_POST['banque']),htmlspecialchars($_POST['categorie']),htmlspecialchars($_POST['role'])
 
